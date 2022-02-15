7db81d84d596312d42a3c30ae135a185d7644825
gyp <2847399255@qq.com>
2022-02-15 21:13:23 +0800

    A

---
+++ include/SyscallId.h
@@ -0,0 +1,10 @@
+#ifndef _SYSCALL_ID_H_
+#define _SYSCALL_ID_H_
+
+#define SYSCALL_PUTCHAR 0
+#define SYSCALL_GET_PROCESS_ID 1
+#define SYSCALL_YIELD 2
+#define SYSCALL_PROCESS_DESTORY 3
+#define SYSCALL_FORK 4
+
+#endif
---
+++ kernel/trap/Syscall.c
@@ -0,0 +1,27 @@
+#include <Syscall.h>
+#include <Driver.h>
+#include <Process.h>
+
+void (*syscallVector[])(void) = {
+    [SYSCALL_PUTCHAR]           syscallPutchar,
+    [SYSCALL_GET_PROCESS_ID]    syscallGetProcessId,
+    [SYSCALL_YIELD]             syscallYield,
+    [SYSCALL_FORK]              syscallFork,
+};
+
+extern Trapframe trapframe[];
+void syscallPutchar() {
+    putchar(trapframe->a0);
+}
+
+void syscallGetProcessId() {
+
+}
+
+void syscallYield() {
+
+}
+
+void syscallFork() {
+
+}
