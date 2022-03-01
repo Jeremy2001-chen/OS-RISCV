80a252ed36131ba1fbde6a2c600cc7f40d576218
GYP <2847399255@qq.com>
2022-03-01 11:01:30 +0800

    A

--- include/Page.h
+++ include/Page.h
@@ -68,6 +68,7 @@ int pageAlloc(PhysicalPage **page);
 int pageInsert(u64 *pgdir, u64 va, u64 pa, u64 perm);
 void pageRemove(u64 *pgdir, u64 va);
 u64 pageLookup(u64 *pgdir, u64 va, u64 **pte);
+int allocPgdir(PhysicalPage **page);
 void pageout(u64 *pgdir, u64 badAddr);
 
 #define IS_RAM(pa) (pa >= PHYSICAL_ADDRESS_BASE && pa < PHYSICAL_MEMORY_TOP)
