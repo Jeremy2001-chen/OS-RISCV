dbfbc18ba858c16ba06b719175c155f0074587e4
ZerglingChen3 <1908216413@qq.com>
2022-03-04 16:46:59 +0800

    Add bootloader

--- include.mk
+++ include.mk
@@ -12,12 +12,12 @@ LD				:= $(CROSS_COMPILE)ld
 LDFLAGS			:= -z max-page-size=4096
 QEMU			:= qemu-system-riscv64
 
-RUSTSBI			:=	./bootloader/aaa
+OPENSBI			:=	./bootloader/dynamic.bin
 
 CPUS := 4
 
 QEMUOPTS = -machine sifive_u -m 1G -nographic
 QEMUOPTS += -smp $(CPUS)
-QEMUOPTS += -bios $(RUSTSBI)
+QEMUOPTS += -bios $(OPENSBI)
 #QEMUOPTS += -drive file=fs.img,if=none,format=raw,id=x0 
 #QEMUOPTS += -device virtio-blk-device,drive=x0,bus=virtio-mmio-bus.0
