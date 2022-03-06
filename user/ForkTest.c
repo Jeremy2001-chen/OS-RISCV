47b532594b507361922eb49505460da588139865
ZerglingChen3 <1908216413@qq.com>
2022-03-06 21:08:12 +0800

    update fork test

--- user/ForkTest.c
+++ user/ForkTest.c
@@ -4,26 +4,28 @@
 
 void userMain(void) {
     uPrintf("start fork test....\n");
-	int id = 0;
-	volatile int i;
-	if ((id = fork()) == 0) {
-		if ((id = fork()) == 0) {
-			syscallPutchar('m');
-			for (i = 0; i < 500; i++) {
-				syscallPutchar('a');
-
-			}
-		} else {
-			syscallPutchar('o');
-			for (i = 0; i < 500; i++) {
-				syscallPutchar('b');
-
-			}
-		}
-	} else {
-        syscallPutchar('n');
-		for (i = 0; i < 500; i++) {
-            syscallPutchar('c');
-		}
-	}
+    int a = 0;
+    int id = 0;
+    if ((id = fork()) == 0) {
+        if ((id = fork()) == 0) {
+            a += 3;
+            if ((id = fork()) == 0) {
+                a += 4;
+                for (;;) {
+                    uPrintf("   this is child3 :a:%d\n", a);
+                }
+            }
+            for (;;) {
+                uPrintf("  this is child2 :a:%d\n", a);
+            }
+        }
+        a += 2;
+        for (;;) {
+            uPrintf(" this is child :a:%d\n", a);
+        }
+    }
+    a++;
+    for (;;) {
+        uPrintf("this is father: a:%d\n", a);
+    }
 }
