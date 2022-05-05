153e88f4413ff5b0ca80c9b9b1db27c544f34d5c
陈纪源 <19373469@buaa.edu.cn>
2022-05-06 01:14:47 +0800

    fix exit

--- user/ExecTest.c
+++ user/ExecTest.c
@@ -11,24 +11,23 @@ void userMain() {
     dup(0); //stderr
     // syscallExec("/yield", argv);
 
-    syscallExec("/exit", argv);
-    // int pid = fork();
-    // if (pid == 0) {
-    //     pid = fork();
-    //     if (pid == 0) {
-    //         pid = fork();
-    //         if (pid == 0) {
-    //             pid = fork();
-    //             syscallExec("/exit", argv);
-    //         }
-    //         else
-    //             syscallExec("/getppid", argv);
-    //     }
-    //     else
-    //         syscallExec("/dup", argv);
-    // } else {
-    //     syscallExec("/getpid", argv);
-    // }
+    // syscallExec("/exit", argv);
+    int pid = fork();
+    if (pid == 0) {
+        pid = fork();
+        if (pid == 0) {
+            pid = fork();
+            if (pid == 0) {
+                syscallExec("/exit", argv);
+            }
+            else
+                syscallExec("/getppid", argv);
+        }
+        else
+            syscallExec("/dup", argv);
+    } else {
+        syscallExec("/getpid", argv);
+    }
     // yield must test finally!!!
     
     // write(1, argv[0], strlen(argv[0]));
