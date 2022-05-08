611fac64e3e65d142f596a4642ddd24ee46752c3
陈纪源 <19373469@buaa.edu.cn>
2022-05-08 17:55:21 +0800

    finish brk

--- kernel/boot/Init.c
+++ kernel/boot/Init.c
@@ -72,8 +72,8 @@ void main(u64 hartId) {
         // PROCESS_CREATE_PRIORITY(SysfileTest, 1);
         // PROCESS_CREATE_PRIORITY(PipeTest, 1);
         // PROCESS_CREATE_PRIORITY(ExecTest,1);
-        PROCESS_CREATE_PRIORITY(ExecToLs, 1);
-        // PROCESS_CREATE_PRIORITY(SyscallTest, 1);
+        // PROCESS_CREATE_PRIORITY(ExecToLs, 1);
+        PROCESS_CREATE_PRIORITY(SyscallTest, 1);
         // PROCESS_CREATE_PRIORITY(WaitTest, 1);
 
     } else {
